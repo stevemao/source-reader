d5855e59bcc4b01c71637c30d37e06c62a228eb8
steve mao <steve.mao@healthinteract.com.au>
2014-03-02 18:31:12 +1100

    load jquery and highlight code. todo: dynamically load sources; use accordion and group code, etc...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$.ajax({
+    url: 'sources/' + 'jQuery-1.11.0' + '.js',
+    success: function(response) {
+        $('pre').html(response);
+
+    }
+});
+// $('a[data-source="jQuery-1.11.0"]').click(function() {
+//     selectSource('jQuery-1.11.0');
+// });
+
+SyntaxHighlighter.all();
