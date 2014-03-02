c951590b6fa08e52f4f8a10a94cf50b3e636e815
steve mao <steve.mao@healthinteract.com.au>
2014-03-03 00:16:05 +1100

    changed source code extension to avoid auto run the code when loaded, made the accordion working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,10 @@
 function selectSource(source) {
     $('.content').html('LOADING...');
     $.ajax({
-        url: 'sources/' + source + '.js',
+        url: 'sources/' + source + '.source',
         success: function(rawCode) {
-            var brushedCode = brushCode(rawCode);
-            setCodeBlock(brushedCode);
+            brushCode(rawCode);
+            setCodeBlock(1, 10, 'Object jQuery');
         }
     });
 }
@@ -15,15 +15,16 @@ function brushCode(rawCode) {
     $('.content').html('<pre class="brush:js">' + rawCode + '</pre>');
     SyntaxHighlighter.defaults['quick-code'] = false;
     SyntaxHighlighter.highlight();
-    return $('.content').html();
 }
 
-function setCodeBlock(brushedCode) {
-    $('.collapse').collapse();
+function setCodeBlock(startline, endline, title) {
+    $('.code .line.number' + startline).before('<h1><a data-toggle="collapse" data-target="#test" href="#">' + title + '</a></h1>');
+    $('.code .line.number' + startline).nextUntil('.code .line.number' + endline).andSelf().wrapAll('<div id="test" class="panel-collapse collapse"></div>');
+    $('#test').collapse();
 }
 
 $('a[data-source="jQuery-1.11.0"]').click(function() {
-    selectSource('jQuery-1.11.0');
+    selectSource('jquery-1.11.0');
 });
 
 $('a[data-source="jQuery-1.10.2"]').click(function() {
