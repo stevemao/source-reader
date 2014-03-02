ef12e539655042264c1b848b310d71d694b5109d
steve mao <steve.mao@healthinteract.com.au>
2014-03-02 21:59:12 +1100

    use *helpers.json to map collapse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,31 @@
 'use strict';
 
-$.ajax({
-    url: 'sources/' + 'jQuery-1.11.0' + '.js',
-    success: function(response) {
-        $('pre').html(response);
+function selectSource(source) {
+    $('.content').html('LOADING...');
+    $.ajax({
+        url: 'sources/' + source + '.js',
+        success: function(rawCode) {
+            var brushedCode = brushCode(rawCode);
+            setCodeBlock(brushedCode);
+        }
+    });
+}
 
-    }
+function brushCode(rawCode) {
+    $('.content').html('<pre class="brush:js">' + rawCode + '</pre>');
+    SyntaxHighlighter.defaults['quick-code'] = false;
+    SyntaxHighlighter.highlight();
+    return $('.content').html();
+}
+
+function setCodeBlock(brushedCode) {
+    $('.collapse').collapse();
+}
+
+$('a[data-source="jQuery-1.11.0"]').click(function() {
+    selectSource('jQuery-1.11.0');
 });
-// $('a[data-source="jQuery-1.11.0"]').click(function() {
-//     selectSource('jQuery-1.11.0');
-// });
 
-SyntaxHighlighter.all();
+$('a[data-source="jQuery-1.10.2"]').click(function() {
+    selectSource('jQuery-1.10.2');
+});
