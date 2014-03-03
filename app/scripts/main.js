cee956630d4c80d684843c02c3226ee3b3c30a9b
steve mao <steve.mao@healthinteract.com.au>
2014-03-03 16:57:11 +1100

    encode html before inserting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,15 +6,33 @@ function selectSource(source) {
         url: 'sources/' + source + '.source',
         success: function(rawCode) {
             brushCode(rawCode);
-            setCodeBlock(1, 10, 'Object jQuery');
+            //setCodeBlock(1, 10, 'Object jQuery');
         }
     });
 }
 
 function brushCode(rawCode) {
-    $('.content').html('<pre class="brush:js">' + rawCode + '</pre>');
+    var encodedCode = '';
+    encodedCode = htmlEncode(rawCode);
+
+    $('.content').html('<pre class="brush:js">' + encodedCode + '</pre>');
     SyntaxHighlighter.defaults['quick-code'] = false;
     SyntaxHighlighter.highlight();
+
+}
+
+
+function htmlEncode(value) {
+    //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+    //then grab the encoded contents back out.  The div never exists on the page.
+    return $('<div/>').text(value).html();
+}
+
+// SyntaxHighlighter generates some extra tags at the end.
+// todo: find out what causes this
+function clearWrongTags(lastLine) {
+    console.log(lastLine);
+    $('.code .line.number9800').html('');
 }
 
 function setCodeBlock(startline, endline, title) {
@@ -28,5 +46,5 @@ $('a[data-source="jQuery-1.11.0"]').click(function() {
 });
 
 $('a[data-source="jQuery-1.10.2"]').click(function() {
-    selectSource('jQuery-1.10.2');
+    selectSource('jquery-1.10.2');
 });
