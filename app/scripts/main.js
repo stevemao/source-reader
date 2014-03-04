ab65a6cff6a4c843879a1516728936ab0b460cb6
steve mao <steve.mao@healthinteract.com.au>
2014-03-05 00:37:09 +1100

    added filter, improved the style and added sizzle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,19 @@ $('a[data-source="jQuery-1.11.0"]').click(function() {
     time = Date.now();
 });
 
+$('#filter').keyup(function(event) {
+    var value = this.value;
+    var $li = $('#menu li');
+    $('#menu li').css('display', 'block').filter(function(index) {
+        if ($(this).children().html().toUpperCase().indexOf(value.toUpperCase()) != -1) {
+            return false;
+        }
+        return true;
+    }).css('display', 'none');
+});
+
 function selectAndBrushCode(source) {
-    $('.content').html('LOADING...');
+    $('.content').html('<pre>LOADING...</pre>');
     $('#menu ul').html('');
     var blocks;
     $.when(
@@ -23,6 +34,7 @@ function selectAndBrushCode(source) {
         })
     )
         .then(function() {
+            $('#rightBar').css('display', 'block');
             blocks.forEach(function(block) {
                 setCodeBlock(block);
             });
@@ -48,21 +60,19 @@ function htmlEncode(value) {
 }
 
 function setCodeBlock(block) {
-    //handle collapse
+    //remove special charactor
+    var id = block.title.replace(/[^a-zA-Z0-9_]/g, '');
     var $title = $('<a></a>', {
-        'href': '#' + block.title,
-        'id': block.title,
-    }).append('<' + block.level + '>' + block.title + '</' + block.level + '>');
+        'href': '#' + id,
+        'id': id,
+    }).append('<' + block.style + '>' + block.title + '</' + block.style + '>');
 
     var $toggle = $title.clone().attr('id', 'toggle_' + block.title);
 
     $toggle.children().html('*');
-    $('.code .line.number' + block.startline).before($title);
-    $('.gutter .line.number' + block.startline).before($toggle);
+    $('.code .line.number' + block.line).before($title);
+    $('.gutter .line.number' + block.line).before($toggle);
 
     //setup menu
-    $('#menu ul').append('<li><a href="#' + block.title + '"><' + block.level + '>' + block.title + '</' + block.level + '></a></li>');
-
-    //handle color
-    //$('.gutter .line.number' + startline).nextUntil('.gutter .line.number' + endline).andSelf().css('background', 'red');
+    $('#menu ul').append('<li><a href="#' + id + '"><' + block.style + '>' + block.title + '</' + block.style + '></a></li>');
 }
